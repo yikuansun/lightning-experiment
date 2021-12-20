39aa25fbaf344636194962ef2d7c8818126d1b03
yikuansun <yikuansun2015@gmail.com>
2021-12-20 11:59:16 -0500

    Turbulent Displace

--- code.js
+++ code.js
@@ -2,12 +2,23 @@ function renderLightning() {
     const svgns = "http://www.w3.org/2000/svg";
     var svgElem = document.querySelector("svg");
 
+    var filters = `
+    <filter id="displacementFilter">
+        <feTurbulence type="turbulence" baseFrequency="0.05"
+            numOctaves="2" result="turbulence"/>
+        <feDisplacementMap in2="turbulence" in="SourceGraphic"
+            scale="50" xChannelSelector="R" yChannelSelector="G"/>
+    </filter>
+    `;
+    svgElem.innerHTML = filters;
+
     var baseLine = document.createElementNS(svgns, "line");
     baseLine.setAttribute("x1", 80);baseLine.setAttribute("x2", 1200);
     baseLine.setAttribute("y1", 80);baseLine.setAttribute("y2", 640);
     baseLine.setAttribute("stroke", "white");
     baseLine.style.strokeWidth = "10px";
     baseLine.style.strokeLinecap = "round";
+    baseLine.style.filter = "url(#displacementFilter)";
     svgElem.appendChild(baseLine);
 }
 
