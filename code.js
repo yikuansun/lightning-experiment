e1a3474b18883488f76f7b43c0b14ad9c2b531c7
yikuansun <yikuansun2015@gmail.com>
2021-12-20 13:41:49 -0500

    Glow effect

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-function renderLightning(indent=100, twitchAmount=169, twitchScale=0.005, twitchOctaves=20, numBranches=5, branchLen=300, branchAngle=Math.PI/4, branchLenDelta=50, softness=2) {
+function renderLightning(indent=100, twitchAmount=169, twitchScale=0.005, twitchOctaves=20, numBranches=5, branchLen=300, branchAngle=Math.PI/4, branchLenDelta=50, softness=2, glowRadius=10, glowColor="#AADDFF") {
     const svgns = "http://www.w3.org/2000/svg";
     var svgElem = document.querySelector("svg");
 
@@ -13,11 +13,22 @@ function renderLightning(indent=100, twitchAmount=169, twitchScale=0.005, twitch
     <filter id="blurFilter">
         <feGaussianBlur in="SourceGraphic" stdDeviation="${softness}" />
     </filter>
+
+    <filter id="glowFilter1">
+        <feFlood result="flood" flood-color="${glowColor}" flood-opacity="1"></feFlood>
+        <feComposite in="flood" result="mask" in2="SourceGraphic" operator="in"></feComposite>
+        <feMorphology in="mask" result="dilated" operator="dilate" radius="2"></feMorphology>
+        <feGaussianBlur in="dilated" result="blurred" stdDeviation="${glowRadius}"></feGaussianBlur>
+        <feMerge>
+            <feMergeNode in="blurred"></feMergeNode>
+            <feMergeNode in="SourceGraphic"></feMergeNode>
+        </feMerge>
+    </filter>
     `;
     svgElem.innerHTML = filters;
 
     var baseGroup = document.createElementNS(svgns, "g");
-    baseGroup.style.filter = "url(#displacementFilter) url(#blurFilter)";
+    baseGroup.style.filter = "url(#displacementFilter) url(#blurFilter) url(#glowFilter1)";
     svgElem.appendChild(baseGroup);
 
     var baseLine = document.createElementNS(svgns, "line");
