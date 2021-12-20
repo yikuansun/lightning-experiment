37262ddb0111ee36e4d3be33c597c7f5b942fdf4
yikuansun <yikuansun2015@gmail.com>
2021-12-20 14:47:48 -0500

    Default collapsed

--- accordion.js
+++ accordion.js
@@ -1,7 +1,12 @@
 for (var sectionHeader of document.querySelectorAll("#options b")) {
+    sectionHeader.style.cursor = "pointer";
     sectionHeader.addEventListener("click", function() {
         var correspondingSection = document.getElementById(this.innerText);
         if (correspondingSection.style.display == "none") correspondingSection.style.display = "block";
         else correspondingSection.style.display = "none";
     });
+}
+
+for (var section of document.querySelectorAll("#options div")) {
+    section.style.display = "none";
 }
