7818bd5339330f6e02b6c1e680da56514093dc36
yikuansun <yikuansun2015@gmail.com>
2022-03-09 23:06:00 -0500

    Remove cursor pointer
    It sucks

--- accordion.js
+++ accordion.js
@@ -1,5 +1,4 @@
 for (var sectionHeader of document.querySelectorAll("#options b")) {
-    sectionHeader.style.cursor = "pointer";
     sectionHeader.addEventListener("click", function() {
         var correspondingSection = document.getElementById(this.innerText);
         if (correspondingSection.style.display == "none") correspondingSection.style.display = "block";
