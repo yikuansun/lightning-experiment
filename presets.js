ed4a854e41c14f37b90b04eea71b1a4c9b29481b
Yikuan Sun <yikuansun2015@gmail.com>
2024-07-30 10:15:49 -0400

    Change beam settings

--- presets.js
+++ presets.js
@@ -16,7 +16,7 @@ function setPreset(presetData) {
 
 var availPresets = {
     "Strike": {"baseLength":1000,"taper":70,"noiseType":"Fractal","twitchAmount":400,"twitchScale":0.005,"twitchOctaves":5,"twitchSeed":8,"numBranches":5,"branchLen":300,"branchLenDelta":54,"branchAngle":33,"coreSize":7,"softness":4,"coreColor":"#ffffff","glowDepth":6,"glowRadius":5,"glowColor":"#00bbff","glowNoiseType":"Fractal","glowTwitchAmount":0,"glowTwitchScale":0.008,"glowTwitchOctaves":7,"glowTwitchSeed":1},
-    "Beam": {"baseLength":600,"taper":100,"noiseType":"Fractal","twitchAmount":42,"twitchScale":0.064,"twitchOctaves":9,"twitchSeed":14,"numBranches":8,"branchLen":210,"branchLenDelta":16,"branchAngle":5,"coreSize":16,"softness":4,"coreColor":"#ffffff","glowDepth":6,"glowRadius":3,"glowColor":"#ff1a7d","glowNoiseType":"Perlin","glowTwitchAmount":131,"glowTwitchScale":0.008,"glowTwitchOctaves":7,"glowTwitchSeed":1},
+    "Beam": {"baseLength":600,"taper":100,"noiseType":"Perlin","twitchAmount":54,"twitchScale":0.064,"twitchOctaves":9,"twitchSeed":14,"numBranches":8,"branchLen":210,"branchLenDelta":16,"branchAngle":5,"coreSize":16,"softness":4,"coreColor":"#ffffff","glowDepth":6,"glowRadius":3,"glowColor":"#ff1a7d","glowNoiseType":"Perlin","glowTwitchAmount":131,"glowTwitchScale":0.008,"glowTwitchOctaves":7,"glowTwitchSeed":1},
 };
 for (var presetName in availPresets) {
     var option = document.createElement("option");
